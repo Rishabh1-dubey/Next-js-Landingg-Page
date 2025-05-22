ad116508cf0913858fb936f954720c51364ade0e
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-05-22 13:52:10 +0530

    adding constent file

---
+++ constents/constents.ts
@@ -0,0 +1,23 @@
+ export const NavLinks = [
+    {
+        id: "1",
+        url: "#",
+        label: "Home"
+    }, {
+        id: "2",
+        url: "#",
+        label: "About"
+    }, {
+        id: "3",
+        url: "#",
+        label: "Contact Us"
+    }, {
+        id: "4",
+        url: "#",
+        label: "Features"
+    }, {
+        id: "5",
+        url: "#",
+        label: "Testinomials"
+    },
+]
